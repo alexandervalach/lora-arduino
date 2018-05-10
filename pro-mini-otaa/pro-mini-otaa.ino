42bb49bbfbca5342acae56efbec8b4d6045eb47f
Alexander Valach <alexander.valach@gmail.com>
2018-05-10 12:26:58 +0200

    Pridanie programov

---
+++ pro-mini-otaa/pro-mini-otaa.ino
@@ -0,0 +1,196 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
+ *
+ * Permission is hereby granted, free of charge, to anyone
+ * obtaining a copy of this document and accompanying files,
+ * to do whatever they want with them without any restriction,
+ * including, but not limited to, copying, modification and redistribution.
+ * NO WARRANTY OF ANY KIND IS PROVIDED.
+ *
+ * This uses OTAA (Over-the-air activation), where where a DevEUI and
+ * application key is configured, which are used in an over-the-air
+ * activation procedure where a DevAddr and session keys are
+ * assigned/generated for use with all further communication.
+ *
+ * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
+ * g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
+ * violated by this sketch when left running for longer)!
+ * To use this sketch, first register your application and device with
+ * the things network, to set or generate an AppEUI, DevEUI and AppKey.
+ * Multiple devices can use the same AppEUI, but each device has its own
+ * DevEUI and AppKey.
+ *
+ * Do not forget to define the radio type correctly in config.h.
+ *
+ *******************************************************************************/
+
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+#include "LowPower.h"
+#include <Arduino.h>
+
+#define sleepcycles 75 
+
+bool joined = false;
+bool sleeping = false;
+
+static const u1_t DEVEUI[8] = { }; // paste value from TTN in LSB
+static const u1_t APPEUI[8] = { }; // paste value from TTN in LSB
+static const u1_t APPKEY[16] = { }; // paste value from TTN in MSB
+
+// provide APPEUI (8 bytes, LSBF)
+void os_getArtEui (u1_t* buf) { memcpy(buf, APPEUI, 8); }
+
+// provide DEVEUI (8 bytes, LSBF)
+void os_getDevEui (u1_t* buf) { memcpy(buf, DEVEUI, 8); }
+
+// provide APPKEY key (16 bytes)
+void os_getDevKey (u1_t* buf) { memcpy(buf, APPKEY, 16); }
+
+static osjob_t sendjob;
+static osjob_t initjob;
+
+// Pin mapping LoRa Dragino Shield
+const lmic_pinmap lmic_pins = {
+.nss = 10,
+.rxtx = LMIC_UNUSED_PIN,
+.rst = 0,
+.dio = {7, 8, 9},
+};
+
+void onEvent (ev_t ev) {
+  switch (ev) {
+    case EV_SCAN_TIMEOUT:
+      Serial.println(F("EV_SCAN_TIMEOUT"));
+      break;
+    case EV_BEACON_FOUND:
+      Serial.println(F("EV_BEACON_FOUND"));
+      break;
+    case EV_BEACON_MISSED:
+      Serial.println(F("EV_BEACON_MISSED"));
+      break;
+    case EV_BEACON_TRACKED:
+      Serial.println(F("EV_BEACON_TRACKED"));
+      break;
+    case EV_JOINING:
+      Serial.println(F("EV_JOINING"));
+      break;
+    case EV_JOINED:
+      Serial.println(F("EV_JOINED"));
+      // Disable link check validation (automatically enabled
+      // during join, but not supported by TTN at this time).
+      LMIC_setLinkCheckMode(0);
+      // after Joining a job with the values will be sent.
+      joined = true;
+      break;
+    case EV_RFU1:
+      Serial.println(F("EV_RFU1"));
+      break;
+    case EV_JOIN_FAILED:
+      Serial.println(F("EV_JOIN_FAILED"));
+      break;
+    case EV_REJOIN_FAILED:
+      Serial.println(F("EV_REJOIN_FAILED"));
+      // Re-init
+      os_setCallback(&initjob, initfunc);
+      break;
+    case EV_TXCOMPLETE:
+      sleeping = true;
+        if (LMIC.dataLen) {
+        // data received in rx slot after tx
+        // if any data received, a LED will blink
+        // this number of times, with a maximum of 10
+        Serial.print(F("Data Received: "));
+        Serial.println(LMIC.frame[LMIC.dataBeg],HEX);
+      }
+      Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+      delay(50);  // delay to complete Serial Output before Sleeping
+
+      // Schedule next transmission
+      // next transmission will take place after next wake-up cycle in main loop
+      break;
+    case EV_LOST_TSYNC:
+      Serial.println(F("EV_LOST_TSYNC"));
+      break;
+    case EV_RESET:
+      Serial.println(F("EV_RESET"));
+      break;
+    case EV_RXCOMPLETE:
+      // data received in ping slot
+      Serial.println(F("EV_RXCOMPLETE"));
+      break;
+    case EV_LINK_DEAD:
+      Serial.println(F("EV_LINK_DEAD"));
+      break;
+    case EV_LINK_ALIVE:
+      Serial.println(F("EV_LINK_ALIVE"));
+      break;
+    default:
+      Serial.println(F("Unknown event"));
+      break;
+  }
+}
+
+void do_send(osjob_t* j) {
+  byte buffer[32];
+
+  String message = "Ahoj, ja som Matt.";
+  message.getBytes(buffer, message.length() + 1);
+  
+  // Check if there is not a current TX/RX job running
+  if (LMIC.opmode & OP_TXRXPEND) {
+    Serial.println(F("OP_TXRXPEND, not sending"));
+  } else {
+    // Prepare upstream data transmission at the next possible time.
+    LMIC_setTxData2(1, (uint8_t*) buffer, message.length() , 0);
+    Serial.println(F("Sending: "));
+  }
+}
+
+// initial job
+static void initfunc (osjob_t* j) {
+    // reset MAC state
+    LMIC_reset();
+    // start joining
+    LMIC_startJoining();
+    // init done - onEvent() callback will be invoked...
+}
+
+void setup()
+  {
+  Serial.begin(9600);
+  delay(250);
+  Serial.println(F("Starting"));
+   
+  LMIC_setClockError(MAX_CLOCK_ERROR * 10 / 100);
+  os_init();
+  // Reset the MAC state. Session and pending data transfers will be discarded.
+  os_setCallback(&initjob, initfunc);
+  LMIC_reset();
+}
+
+unsigned long time;
+void loop()
+{
+    // start OTAA JOIN
+    if (joined==false)
+    {
+      os_runloop_once();
+    }
+    else
+    {
+      do_send(&sendjob);    // Sent sensor values
+      while(sleeping == false)
+      {
+        os_runloop_once();
+      }
+      sleeping = false;
+      for (uint8_t i=0; i<sleepcycles;i++)
+      {
+          LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);    //sleep 8 seconds
+      }
+    }
+}
